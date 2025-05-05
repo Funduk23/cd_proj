611d65d958622086e2082e09b41f72ffb6da1831
Funduk23 <funduk171@gmail.com>
2025-05-05 21:49:38 +0300

    Rework pr5 and added pr7

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from promethus_flask_exporter import PrometheusMetrics
+import logging
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app, group_by='endpoint')
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @app.route('/')
 def home():
+    logger.info('Received request on home page')
     return 'Hello, Continuous Deployment!'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
