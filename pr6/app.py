4e4b6cab75d60a3008b8d56c1bf053372afabfcf
Funduk23 <funduk171@gmail.com>
2025-05-05 11:34:31 +0300

    Monitoring tune

--- pr6/app.py
+++ pr6/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request
+from prometheus_flask_exporter import PrometheusMetrics
 import logging
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app, group_by='endpoint')
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -9,7 +11,10 @@ logger = logging.getLogger(__name__)
 @app.route('/')
 def home():
     logger.info('Received request on home page')
-    return 'Hello, Monitoring!'
+    return 'Hello and bye, Monitoring!'
+@app.route('/health')
+def health():
+    return "Healty", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
